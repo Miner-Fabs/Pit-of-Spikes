142cd532874cca45259718ed071fdb10b66b79e9
Fabian-Skyner <40596430@live.napier.ac.uk>
2026-04-01 18:01:56 +0100

    began proper settings menu implementation
    added sliders to settings menu, an options script, moved sound manager script to empty object, and made a volume for brightness

--- Pit of Spikes v1.1/Assets/Scripts/OptionsScript.cs
+++ Pit of Spikes v1.1/Assets/Scripts/OptionsScript.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class OptionsScript : MonoBehaviour
 {
+    //private PlayerInputActions pInput;
+    //[SerializeField]
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //pInput = new PlayerInputActions();
     }
 
     // Update is called once per frame
