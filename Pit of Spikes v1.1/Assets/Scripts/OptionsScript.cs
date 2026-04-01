057ca2c2422364d66ac3c0089903dfc9d47fa42f
Fabian-Skyner <40596430@live.napier.ac.uk>
2026-04-01 21:55:17 +0100

    Added audio and quality settings
    added more settings to options menu, removed brightness setting

--- Pit of Spikes v1.1/Assets/Scripts/OptionsScript.cs
+++ Pit of Spikes v1.1/Assets/Scripts/OptionsScript.cs
@@ -1,18 +1,49 @@
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
+using UnityEngine.Audio;
+using TMPro;
 
 public class OptionsScript : MonoBehaviour
 {
-    //private PlayerInputActions pInput;
-    //[SerializeField]
+    public TMP_Dropdown graphicsDropdown;
+
+    public Slider musicVol, sfxVol;
+    public AudioMixer mainAudioMixer;
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+
+    public void ChangeMusicVolume()
+    {
+        if (musicVol.value <= -60f)
+        {
+            mainAudioMixer.SetFloat("MusicVolume", -80f);
+        }
+        else
+        {
+            mainAudioMixer.SetFloat("MusicVolume", musicVol.value);
+        }
+    }
+
+    public void ChangeSFXVolume()
+    {
+        if (sfxVol.value <= -60f)
+        {
+            mainAudioMixer.SetFloat("SFXVolume", -80f);
+        }
+        else
+        {
+            mainAudioMixer.SetFloat("SFXVolume", sfxVol.value);
+        }
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //pInput = new PlayerInputActions();
+        
     }
 
     // Update is called once per frame
